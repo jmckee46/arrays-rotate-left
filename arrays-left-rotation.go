2895958705ae47c0a928abfd32ee1dd3f112430b
John McKee <jmckee3@mac.com>
2019-11-22 16:27:22 -0800

    initial commit

---
+++ arrays-left-rotation.go
@@ -0,0 +1,8 @@
+package main
+
+func rotLeft(a []int32, d int32) []int32 {
+
+	return nil
+}
+
+func main() {}
---
+++ spec-arrays-left-rotation_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestRotLeft(t *testing.T) {
+
+}
