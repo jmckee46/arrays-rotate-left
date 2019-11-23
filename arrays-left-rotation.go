c86ff7138068768f691b3de56e3b8eb8d2ebccc1
John McKee <jmckee3@mac.com>
2019-11-22 17:08:35 -0800

    working version

--- arrays-left-rotation.go
+++ arrays-left-rotation.go
@@ -1,8 +1,16 @@
 package main
 
 func rotLeft(a []int32, d int32) []int32 {
+	aLength := int32(len(a))
+	var shift int32
 
-	return nil
+	if d >= aLength {
+		shift = d % aLength
+	} else {
+		shift = d
+	}
+
+	return append(a[shift:len(a):len(a)], a[:shift]...)
 }
 
 func main() {}
--- spec-arrays-left-rotation_test.go
+++ spec-arrays-left-rotation_test.go
@@ -1,7 +1,24 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestRotLeft(t *testing.T) {
+func TestRotLeft1(t *testing.T) {
+	a := []int32{1, 2, 3, 4, 5}
+	d := int32(4)
 
+	returnedArray := rotLeft(a, d)
+
+	fmt.Println("returnedArray", returnedArray)
+}
+
+func TestRotLeft2(t *testing.T) {
+	a := []int32{1, 2, 3, 4, 5}
+	d := int32(13)
+
+	returnedArray := rotLeft(a, d)
+
+	fmt.Println("returnedArray", returnedArray)
 }
